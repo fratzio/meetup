57bb37a5dae95882ab316b600af91865e08d1925
fratzio <thomasmichaelfrazer@gmail.com>
2020-07-27 22:56:22 -0700

    Updates

---
+++ precache-manifest.78c78c513494dc4afbd48d46c64d4d06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61fe7e3162ce140944fb19d9d2f25fae",
+    "url": "/meetup/index.html"
+  },
+  {
+    "revision": "6cd062f68dc39389e2ec",
+    "url": "/meetup/static/css/main.9c8c006a.chunk.css"
+  },
+  {
+    "revision": "5ca2ad8a51bc4a8e6fa8",
+    "url": "/meetup/static/js/2.a39ddf80.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/meetup/static/js/2.a39ddf80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd062f68dc39389e2ec",
+    "url": "/meetup/static/js/main.218f9b2d.chunk.js"
+  },
+  {
+    "revision": "5b15bedc16c52474edcc",
+    "url": "/meetup/static/js/runtime-main.61f01ad6.js"
+  },
+  {
+    "revision": "05724c0ed0ade783d133b3e987435439",
+    "url": "/meetup/static/media/river_photo.05724c0e.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.c422ef3860b85a877db6aba9f7c5a092.js"
+  "/meetup/precache-manifest.78c78c513494dc4afbd48d46c64d4d06.js"
 );
 
 self.addEventListener('message', (event) => {
